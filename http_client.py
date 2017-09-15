cb9c494f5ebd450ac3ee74d3f0cf498cdf163022
chooyan-eng <chooyan.enginner@gmail.com>
2017-09-15 18:27:22 +0900

    [modify] send first line of request

--- http_client.py
+++ http_client.py
@@ -8,6 +8,9 @@ class ChooyanHttpClient:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
 
+        request_str = 'GET / HTTP/1.1\nHost: %s\r\n\r\n' % (host)
+        s.send(request_str.encode('utf-8'))
+
         return response
 
 class ChooyanResponse:
