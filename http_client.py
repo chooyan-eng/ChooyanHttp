bf66d142da599e9a54dd8a2d1b0adf4e3a372b2b
chooyan-eng <chooyan.enginner@gmail.com>
2017-09-15 18:23:06 +0900

    [modify] skelton of ChooyanHttpClient

--- http_client.py
+++ http_client.py
@@ -1,3 +1,14 @@
+class ChooyanHttpClient:
+
+    def request(host, port=80):
+        response = ChooyanResponse()
+        return response
+
+class ChooyanResponse:
+    def __init__(self):
+        self.responce_code = None
+        self.body = None
+
 if __name__ == '__main__':
     resp = ChooyanHttpClient.request('www.hasam.jp', 80)
     if resp.responce_code == 200:
