787aae44c96d49ddec198111e675ed8638787e43
chooyan-eng <chooyan.enginner@gmail.com>
2017-09-15 18:24:57 +0900

    [modify] connection to the server

--- http_client.py
+++ http_client.py
@@ -1,7 +1,13 @@
+import socket
+
 class ChooyanHttpClient:
 
     def request(host, port=80):
         response = ChooyanResponse()
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+
         return response
 
 class ChooyanResponse:
